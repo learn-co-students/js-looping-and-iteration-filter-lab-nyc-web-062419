64e6e27ebb1a73f9ab58b8c6ad03144ae93f421f
Colin <cstatro@gmail.com>
2019-08-03 18:19:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+let test = ["grumpy angel", "little bob", "poopsie"]
+function findMatching(drivers, name) {
+    return drivers.filter(val => val.toUpperCase() === name.toUpperCase())
+}
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(val => val.indexOf(string) === 0)
+}
+function matchName(drivers, string) {
+    return drivers.filter(val => val.name === string)
+}
+
+
+
+console.log(fuzzyMatch(test, "grum"));
