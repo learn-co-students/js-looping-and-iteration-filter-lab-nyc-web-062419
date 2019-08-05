84b5a16e3baf939a9be7081bd5c1747d464ad330
alexvirga <alexander.virga@gmail.com>
2019-08-04 21:08:17 -0400

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function findMatching (drivers, name) {
         return driver.toLowerCase() === name.toLowerCase()
     })
 }
+
+function fuzzyMatch(drivers, letters){
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().split("")[0,1] === letters.toLowerCase().split("")[0,1];
+    })
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.name.toLowerCase() === name.toLowerCase();
+    })
+}
